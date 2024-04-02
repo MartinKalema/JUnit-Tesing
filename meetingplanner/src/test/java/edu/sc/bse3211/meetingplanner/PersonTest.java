0548bad9a2eb4fe340498abff041b51480adc8cb
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-04-02 15:42:53 +0300

    Person module tests complete

--- meetingplanner/src/test/java/edu/sc/bse3211/meetingplanner/PersonTest.java
+++ meetingplanner/src/test/java/edu/sc/bse3211/meetingplanner/PersonTest.java
@@ -4,6 +4,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PersonTest {
-	// Add test methods here. 
-    // You are not required to write tests for all classes.
+	@Test
+    public void testDefaultConstructor() {
+        // **Test Description:** Verifies the behavior of the default constructor in the Person class.
+        //                       It ensures that a new Person object is created with an empty name.
+        Person person = new Person();
+        assertEquals("", person.getName());
+    }
+
+    @Test
+    public void testParameterizedConstructor() {
+        // **Test Description:** Verifies the behavior of the parameterized constructor in the Person class.
+        //                       It ensures that a new Person object is created with the provided name.
+        Person person = new Person("Shiphra");
+        assertEquals("Shiphra", person.getName());
+    }
+
+    @Test
+    public void testNullNameConstructor() {
+        // **Test Description:** Verifies the behavior of the constructor in the Person class when a null name is provided.
+        //                       It ensures that a new Person object is created with a null name.
+        Person person = new Person(null);
+        assertNull(person.getName());
+    }
+
+
 }
